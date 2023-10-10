929c395566a9d662075bf1c6b6d6652448884705
FaustoPino <nicolasfausto5@gmail.com>
2023-10-10 20:28:04 -0300

    CAMBIOS GUARDADOS

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -23,7 +23,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const [errorType, setErrorType] = useState('');
-
+//RECORDATORIO!!! validar existencia de correo
     const signup = (email, password) =>{
         createUserWithEmailAndPassword(auth, email, password);
         cookies.set('token', 'nombre xd', {path: "/"})
